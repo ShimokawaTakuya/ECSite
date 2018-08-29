2cea117fcc9d0d53c299a2c27fd55c805f527227
ShimokawaTakuya <1114605@alumni.tus.ac.jp>
2018-08-29 20:09:24 +0900

    2018/08/29

---
+++ template/src/com/internousdev/template/util/DateUtil.java
@@ -0,0 +1,17 @@
+package com.internousdev.template.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+	public String getDate() {
+
+		Date date=new Date();
+		SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy/MM/dd HH:ss");
+
+		return simpleDateFormat.format(date);
+
+	}
+
+}
